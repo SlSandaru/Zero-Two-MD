cb46de442559a7cde8e59f60086aaed8061b533a
SlSandaru <120253022+SlSandaru@users.noreply.github.com>
2024-03-05 11:19:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id',
+SESSION_ID: process.env.SESSION_ID || 'ZEROTWO=JyEnCSTR#oZIbNckyDiLJrfTnXNVTKN4sIS9zNhwTVxBRfREk53o',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'youre POSTGRESQL url',
 LANG: process.env.BOT_LANG || 'EN' ,
-ANTI_BAD: process.env.ANTI_BAD || 'false',
+ANTI_BAD: process.env.ANTI_BAD || 'true',
 MAX_SIZE: process.env.MAX_SIZE || 200,
 ONLY_GROUP: process.env.ONLY_GROUP || 'false',
-ANTI_LINK: process.env.ANTI_LINK || 'false' ,
+ANTI_LINK: process.env.ANTI_LINK || 'true' ,
 ANTI_BOT: process.env.ANTI_BOT || 'false',
 ALIVE: process.env.ALIVE || `default`,
 FOOTER: process.env.FOOTER ||  '©𝚉𝙴𝚁𝙾-𝚃𝚆𝙾 𝙼𝙳',
