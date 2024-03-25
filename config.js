57c5dba6f0bf2864585073e880e7831a641c7da6
SlSandaru <120253022+SlSandaru@users.noreply.github.com>
2024-03-25 22:18:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'ZEROTWO=JyEnCSTR#oZIbNckyDiLJrfTnXNVTKN4sIS9zNhwTVxBRfREk53o',
+SESSION_ID: process.env.SESSION_ID || 'ZEROTWO=V7NzjZDK#bhRZcW30nL94UdrwyM3F3-NDqQeJO-Fppi470qkpRdo',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://sandaru_user:tXGXvmE9u58NbkJ6OvxO9p3RspXss2ZK@dpg-cnjbc6n109ks73bneq80-a.frankfurt-postgres.render.com/sandaru',
 LANG: process.env.BOT_LANG || 'EN' ,
 ANTI_BAD: process.env.ANTI_BAD || 'true',
-MAX_SIZE: process.env.MAX_SIZE || 700,
+MAX_SIZE: process.env.MAX_SIZE || 500,
 ONLY_GROUP: process.env.ONLY_GROUP || 'false',
 ANTI_LINK: process.env.ANTI_LINK || 'true' ,
 ANTI_BOT: process.env.ANTI_BOT || 'false',
